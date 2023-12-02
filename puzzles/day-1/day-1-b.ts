2bb8bb82bc7855b22a4de1b2f7df75f794b31f66
Daniel Lauzon <daniel.lauzon@gmail.com>
2023-12-02 04:49:01 -0500

    da-1-b

--- puzzles/day-1/day-1-b.ts
+++ puzzles/day-1/day-1-b.ts
@@ -3,7 +3,68 @@ import chalk from 'chalk';
 
 export async function day1b(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+  console.log({ data });
+  const sum = data
+    .map((line) => {
+      const digits: number[] = []; // accumulated digits
+      const dWords = [
+        'zero',
+        'one',
+        'two',
+        'three',
+        'four',
+        'five',
+        'six',
+        'seven',
+        'eight',
+        'nine',
+      ];
+      console.log('---', { line });
+      let lineRemain = line;
+      outer: while (lineRemain.length > 0) {
+        // 1- if line starts with a digit
+        if (lineRemain.match(/^\d/)) {
+          const d = Number(lineRemain.slice(0, 1));
+          digits.push(d);
+          // console.log('d', { lineRemain, d });
+
+          lineRemain = lineRemain.slice(1);
+          continue;
+        }
+        // 2- if line starts with a digit word
+        // we should only remove one letter in case there are nested letters
+        for (const dWord of dWords) {
+          const i = lineRemain.indexOf(dWord);
+          if (i === 0) {
+            const d = dWords.indexOf(dWord);
+            // lineRemain = lineRemain.slice(dWord.length);
+            lineRemain = lineRemain.slice(1);
+
+            // console.log('w', { lineRemain, dWord, d });
+            digits.push(d);
+            continue outer;
+          }
+        }
+        // else not a digit or digit word
+        // remove the first char and try again
+        lineRemain = lineRemain.slice(1);
+        // console.log('...', { lineRemain });
+      }
+      if (digits.length > 0) {
+        const n = digits[0] * 10 + digits[digits.length - 1];
+        console.log('+++', { digits: digits.join(''), n });
+        return n;
+      }
+      console.log('no digits found');
+      process.exit(1);
+      return 0;
+    })
+    .reduce((acc, n) => {
+      return acc + n;
+    }, 0);
+
+  console.log({ sum });
+  return sum;
 }
 
 const answer = await day1b();
