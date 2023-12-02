2f58aecd63697806326d39c474b31d172b96d89a
Daniel Lauzon <daniel.lauzon@gmail.com>
2023-12-02 13:25:07 -0500

    day-2

--- puzzles/day-2/day-2-a.ts
+++ puzzles/day-2/day-2-a.ts
@@ -1,9 +1,50 @@
+import { parse } from 'path';
 import { readData } from '../../shared.ts';
 import chalk from 'chalk';
 
+// global since this is constant
+const inBagPerColor = { red: 12, green: 13, blue: 14 };
+
 export async function day2a(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+  // bag has only 12 red cubes, 13 green cubes, and 14 blue cubes
+  console.log({ data });
+  const sum = data.map(isGamePossible).reduce((acc, n) => {
+    return acc + n;
+  }, 0);
+
+  return sum;
+}
+
+function isGamePossible(line: string) {
+  console.log({ line });
+  // { line: 'Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green' }
+  const parts = line.split(':');
+  const gameID = Number(parts[0].slice(5));
+  const possible = parts[1]
+    .split(';')
+    .map((countForDraw) => {
+      return countForDraw
+        .trim()
+        .split(',')
+        .map((countForColor) => {
+          const [n, color] = countForColor.trim().split(' ');
+          return { n: Number(n), color };
+        })
+        .map(({ n, color }) => {
+          // check if this count is possible (are there enough in the bag)
+          const inBag = inBagPerColor[color];
+          if (n > inBag) {
+            console.log('not enough', { n, color, inBag });
+          }
+          return n <= inBag;
+        })
+        .every((x) => x);
+    })
+    .every((x) => x);
+
+  console.log('G', gameID, possible);
+  return possible ? gameID : 0;
 }
 
 const answer = await day2a();
--- puzzles/day-2/day-2-b.ts
+++ puzzles/day-2/day-2-b.ts
@@ -3,7 +3,40 @@ import chalk from 'chalk';
 
 export async function day2b(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+  // bag has only 12 red cubes, 13 green cubes, and 14 blue cubes
+  console.log({ data });
+  const sum = data.map(parseLine).reduce((acc, n) => {
+    return acc + n;
+  }, 0);
+
+  return sum;
+}
+
+function parseLine(line: string) {
+  const minsForGame = { red: 0, green: 0, blue: 0 };
+  console.log({ line });
+  // { line: 'Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green' }
+  const parts = line.split(':');
+  const gameID = Number(parts[0].slice(5));
+  const countsPerDraw = parts[1].split(';').map((countForDraw) => {
+    return countForDraw
+      .trim()
+      .split(',')
+      .map((countForColor) => {
+        const [nStr, color] = countForColor.trim().split(' ');
+        const n = Number(nStr);
+        if (minsForGame[color] < n) {
+          // console.log('new min', { n, color });
+          minsForGame[color] = n;
+        }
+        return { n, color };
+      });
+  });
+
+  // console.log('G', gameID, countsPerDraw);
+  const power = minsForGame.red * minsForGame.green * minsForGame.blue;
+  console.log('Pow', gameID, power, minsForGame);
+  return power;
 }
 
 const answer = await day2b();
