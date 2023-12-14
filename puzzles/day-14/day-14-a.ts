9986cc5b467eb8d65c111d6b6b0f5dca09d7ffe2
Daniel Lauzon <daniel.lauzon@gmail.com>
2023-12-14 00:29:30 -0500

    day-14-a

--- puzzles/day-14/day-14-a.ts
+++ puzzles/day-14/day-14-a.ts
@@ -1,13 +1,50 @@
 import { readData } from '../../shared.ts';
 import chalk from 'chalk';
 
+function transpose(lines: string[]) {
+  return lines[0]
+    .split('')
+    .map((_, i) => lines.map((row) => row[i]))
+    .map((row) => row.join(''));
+}
+
+//  example lines
+// { row: 0, line: 'OO.O.O..##' }
+// { row: 1, line: '...OO....O' }
+// { row: 2, line: '.O...#O..O' }
+// { row: 3, line: '.O.#......' }
+// O can slide left, if there is a . to the left
+// # cannot move
+// .O -> O.
+// ..O -> O..
+// .OO -> OO.
+// .O.O -> OO..
+function slideLeft(line: string): string {
+  // count the 'O's
+  const countO = line.split('').filter((c) => c === 'O').length;
+  return line
+    .split('')
+    .map((c, i) => (i < countO ? 'O' : '.'))
+    .join('');
+}
 export async function day14a(dataPath?: string) {
   const data = await readData(dataPath);
-  return data
+  const transposed = transpose(data);
+  return transposed
     .map((line: string, row) => {
-      const lines = line.split('\n');
-      console.log({ row, line });
-      return 1;
+      console.log('-', { row, line });
+      const sl = line
+        .split('#')
+        .map((part) => slideLeft(part))
+        .join('#');
+      console.log('+', { row, line: sl });
+      const load = sl
+        .split('')
+        .map((c, i) => {
+          return c === 'O' ? line.length - i : 0;
+        })
+        .reduce((acc, cur) => acc + cur, 0);
+      return load;
     })
     .reduce((acc, cur) => acc + cur, 0);
   // return 0;
