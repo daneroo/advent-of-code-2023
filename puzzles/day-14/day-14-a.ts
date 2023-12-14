e6e6b3841b62b882d242d6f207fe15b256b13a39
Daniel Lauzon <daniel.lauzon@gmail.com>
2023-12-13 20:29:46 -0500

    ready for day-14

--- puzzles/day-14/day-14-a.ts
+++ puzzles/day-14/day-14-a.ts
@@ -3,6 +3,14 @@ import chalk from 'chalk';
 
 export async function day14a(dataPath?: string) {
   const data = await readData(dataPath);
+  return data
+    .map((line: string, row) => {
+      const lines = line.split('\n');
+      console.log({ row, line });
+      return 1;
+    })
+    .reduce((acc, cur) => acc + cur, 0);
+  // return 0;
   return 0;
 }
 
